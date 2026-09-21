f654713c1ecd6e9e37a163932db3c1fed6bd48cb
Marimar E. Mariñas <marimarmarinas14@gmail.com>
2026-09-20 21:52:43 -0700

    Add files via upload

---
+++ Main.java
@@ -0,0 +1,28 @@
+class Main {
+           
+            public static void main(String [] args) {
+            
+            Movie m1 = new Movie();
+            
+            m1.title = "Maleficient";
+            m1.genre = "Fantasy";
+            m1.duration = 120;
+            m1.displayInfo();
+            
+            Movie m2 = new Movie();
+ 
+            m2.title = "Moana";
+            m2.genre = "Fantasy";
+            m2.duration = 115;
+            m2.displayInfo();
+         
+            Movie m3 = new Movie();
+
+            m3.title = "Marita";
+            m3.genre = "Horror";
+            m3.duration = 106;
+            m3.displayInfo();
+  
+  
+           }
+ }          
